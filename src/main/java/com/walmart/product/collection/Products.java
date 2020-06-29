5b7e8c84b42c1994843d88b27f4bb114e1acb144
=Daniel Hernandez <=df.hernandez@hotmail.cl>
2020-06-28 22:25:53 -0400

    pre refactor

--- src/main/java/com/walmart/product/collection/Products.java
+++ src/main/java/com/walmart/product/collection/Products.java
@@ -17,4 +17,18 @@ public class Products {
     //Fields only use in palindrome logic, does not persist in bd
     private Boolean isPalindrome = false;
     private int pricePalindrome = 0;
+
+    public Products(String _id, int id, String brand, String description, String image, int price, Boolean isPalindrome,
+            int pricePalindrome) {
+        this._id = _id;
+        this.id = id;
+        this.brand = brand;
+        this.description = description;
+        this.image = image;
+        this.price = price;
+        this.isPalindrome = isPalindrome;
+        this.pricePalindrome = pricePalindrome;
+    }
+
+    
 }
--- src/main/java/com/walmart/product/controller/ProductController.java
+++ src/main/java/com/walmart/product/controller/ProductController.java
@@ -6,11 +6,13 @@ import com.walmart.product.collection.Products;
 import com.walmart.product.service.IProductService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("api/product")
 public class ProductController {
